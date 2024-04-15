f0cfd427e16d53b0e546ce25b04563d5b4f3d853
NimoMohamed <nmohamed@macalester.edu>
2024-04-15 18:10:13 -0500

    Initilized the heap

--- src/Huffman.java
+++ src/Huffman.java
@@ -2,21 +2,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Huffman {
-    
 
-    public Huffman(String input){
+
+    public Huffman(String input) {
 
     }
 
-    public Map<Character, Integer> readFile(String input){
+    public Map<Character, Integer> readFile(String input) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
         char[] charInput = input.toCharArray();
-        for (char eachChar: charInput){
-            if (frequencyMap.get(eachChar)!= null){
+        for (char eachChar : charInput) {
+            if (frequencyMap.get(eachChar) != null) {
                 Integer freq = frequencyMap.get(eachChar);
-                freq= freq+1;
-            }
-            else{
+                freq = freq + 1;
+            } else {
                 frequencyMap.put(eachChar, 1);
             }
         }
