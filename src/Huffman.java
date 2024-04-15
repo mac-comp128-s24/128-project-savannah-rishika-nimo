f101611baf66e50a4126a7cebc7030fedc9206aa
savhaugen <shaugen@macalester.edu>
2024-04-15 11:33:18 -0500

    working

---
+++ src/Huffman.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Huffman {
+    
+
+    public Huffman(String input){
+
+    }
+
+    public Map<Char, Integer> readFile(String input){
+        Map<Char, Integer> frequencyMap = new HashMap<>();
+        Char[] charInput = input.toCharArray();
+        for (Character char: input){
+
+        }
+        
+        
+        
+        
+        return frequencyMap;
+        
+
+    }
+}
