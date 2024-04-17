04c8e351daafbebb0b4f3ce88e5742fe477a522e
RishikaKundu <144388152+RishikaKundu@users.noreply.github.com>
2024-04-17 11:25:46 -0500

    Update FileGUI.java

--- src/FileGUI.java
+++ src/FileGUI.java
@@ -5,6 +5,7 @@ import edu.macalester.graphics.GraphicsObject;
 import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.Image;
 import edu.macalester.graphics.Point;
+import edu.macalester.graphics.ui.Button;
 import edu.macalester.graphics.ui.TextField;
 import java.awt.Color;
 
@@ -13,11 +14,13 @@ public class FileGUI {
     private CanvasWindow canvas;
     private TextField inputText;
     private GraphicsText title;
+    private Button addTextButton;
 
     public FileGUI() {
         canvas = new CanvasWindow("File Compressor Program", 900, 800);
         inputText = new TextField();
         title = new GraphicsText();
+        addTextButton = new Button("Compress Text");
         setUpUI();
 
 
@@ -26,13 +29,21 @@ public class FileGUI {
     public void setUpUI() {
         inputText.setPosition(450, 400);
         inputText.setBackground(Color.LIGHT_GRAY);
+
         title.setText("File Compressor Program");
         title.setFont(FontStyle.BOLD_ITALIC, 40);
         title.setPosition(200, 75);
+
         canvas.add(title);
         canvas.add(inputText);
+
+        addTextButton.setPosition(600, 400);
+        canvas.add(addTextButton);
+        addTextButton.onClick(() -> System.out.println(inputText.getText()));
+
+
         String str = inputText.getText();
-        System.out.println("str" + str);
+
     }
 
     public static void main(String[] args) {
