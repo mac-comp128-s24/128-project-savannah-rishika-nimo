6310614b4d5f43f6c46cf4362488f72d3fef5f8f
RishikaKundu <144388152+RishikaKundu@users.noreply.github.com>
2024-04-17 18:57:57 -0500

    Update Heap.java

--- src/Heap.java
+++ src/Heap.java
@@ -2,20 +2,23 @@ import java.util.*;
 
 public class Heap {
 
-    PriorityQueue<Integer> queue;
+    PriorityQueue<Node> queue;
 
     public Heap() {
         queue = new PriorityQueue<>(Collections.reverseOrder());
-        queue.add(1);
-        queue.add(5);
-        queue.add(4);
-        queue.add(3);
-        queue.add(6);
+
+        // queue.add(1);
+        // queue.add(5);
+        // queue.add(4);
+        // queue.add(3);
+        // queue.add(6);
+        // [6, 5, 4, 1, 3]
 
 
     }
 
     public void print() {
+        queu
         System.out.println(queue);
     }
 
